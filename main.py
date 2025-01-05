32f9ffd410264ad22c0fa1a94804acaf86941d64
Iraklis Markelis <ir.markelis@hotmail.com>
2025-01-05 23:30:06 +0200

    Added working code

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://fragtrag1.upatras.gr/exist/apps/fragtrag/indexc.html"
+
+grab = requests.get(url)
+soup = BeautifulSoup(grab.content, 'html.parser')
+# rs = (grequests.get(u) for u in url)
+
+
+"""
+rs = grequests.get(url)
+
+for r in grequests.map(rs):
+    print(f'r.url: {r.url}')
+"""
+
+f = open('urls.txt', 'w')
+
+for link in soup.find_all('a'):
+    data = link.get('href')
+    if '.xml' in data:
+        f.write(data)
+        f.write('\n')
+
+f.close()
